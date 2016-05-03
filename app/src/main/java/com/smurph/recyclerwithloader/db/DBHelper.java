1dc8dcb4b662695e7e6799b7484533f66a93987e
Ben Murphy <s.murph82@gmail.com>
2016-05-03 15:11:26 -0400

    Added some update code to the upgrade method this will help when you need to edit the local SQLite db.

--- app/src/main/java/com/smurph/recyclerwithloader/db/DBHelper.java
+++ app/src/main/java/com/smurph/recyclerwithloader/db/DBHelper.java
@@ -3,6 +3,8 @@ package com.smurph.recyclerwithloader.db;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Config;
+import android.util.Log;
 
 /**
  * Created by Ben on 5/3/2016.
@@ -10,6 +12,7 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DBHelper extends SQLiteOpenHelper {
 
+    private static final String TAG = DBHelper.class.getSimpleName();
 
     private static final int CURRENT_VERSION = 1;
     private static final String DB_NAME = "mydb.db";
@@ -23,6 +26,28 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion >= newVersion) {
+            Log.i(TAG, "DB is current version.");
+            return;
+        }
 
+        db.beginTransaction();
+        try {
+            Log.i(TAG, "Starting DB update. From: " + oldVersion + " to: " + newVersion);
+            // NOTE: 5/3/2016 That ++i is used instead of i++. This is because we want the i value
+            // to increment before we start the loop.
+            for (int i = oldVersion; i < newVersion; ++i) {
+                int nextVersion = i + 1;
+                switch (nextVersion) {
+                    default:
+                        Log.i(TAG, "No DB updates");
+                        break;
+                }
+            }
+            db.setTransactionSuccessful();
+            Log.i(TAG, "DB update successful.");
+        }
+        catch (Exception e) { e.printStackTrace(); }
+        finally { db.endTransaction(); }
     }
 }
